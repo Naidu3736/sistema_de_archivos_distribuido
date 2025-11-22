7f0a4f9a0978013454f6059df7b9548fd4b39db5
Francisco Aparicio Martínez <franciscoapa9693736@gmail.com>
2025-11-21 20:28:52 -0600

    Error en el método de desconectar

--- client/file_client.py
+++ client/file_client.py
@@ -45,7 +45,7 @@ class FileClient:
     def disconnect(self):
         """Cierra la conexión con el servidor"""
         if self.socket:
-            self.socket.send(Command.DELETE.to_bytes())
+            self.socket.send(Command.DISCONNECT.to_bytes())
             self.socket.close()
             self.socket = None
             self.is_connected = False
